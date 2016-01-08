9aa6ea4b49cb4ab898793d61ad4b213af723e6a5
Alec <akosik7@gmail.com>
2016-01-08 16:50:53 -0500

    Merger completed

--- js/createEvent.js
+++ js/createEvent.js
@@ -1,31 +1,30 @@
 var btn = document.getElementById("btn");
 btn.onclick = function()
-{
-  navigator.geolocation.getCurrentPosition(function(pos)
-  {
-      var myLat= pos.coords.latitude;
-      var myLng= pos.coords.longitude;
-      var title= document.getElementById("title").value;
-      var description = document.getElementById("description").value;
-      var type = document.getElementById("type").value;
-    var msg = {"title": title, "location": {"type": "Point", "coordinates":[ myLng, myLat ]}, "description": description,
-      "eventType": type, "startTime":Date.now(),"locationHint": "Library Lobby"};
-    $.ajax(
       {
-        type: 'POST',
-        url:'/createEvent',
-        data: JSON.stringify(msg),
-        dataType: 'json',
-        success: function(result,status)
-        {
-            location.href='/pages/mapPage.html';
-        },
-        error: function(jqXHR,textStatus,errorThrown)
-          {
-              location.href='/pages/mapPage.html';
-              console.log(textStatus,errorThrown);
-              alert("Your event could not be created at this time.");
-        }
-      });
-  });
-};
+          navigator.geolocation.getCurrentPosition(function(pos)
+                                                   {
+                                                       var myLat= pos.coords.latitude;
+                                                       var myLng= pos.coords.longitude;
+                                                       var title= document.getElementById("title").value;
+                                                       var description = document.getElementById("description").value;
+                                                       var type = document.getElementById("type").value;
+                                                       var msg = {"title": title, "location": {"type": "Point", "coordinates":[ myLng, myLat ]}, "description": description,
+                                                                  "eventType": type, "startTime":Date.now(),"locationHint": "Library Lobby"};
+                                                       $.ajax(
+                                                           {
+                                                               type: 'POST',
+                                                               url:'/createEvent',
+                                                               data: JSON.stringify(msg),
+                                                               dataType: 'json',
+                                                               success: function(result,status)
+                                                               {
+                                                                   location.href='/pages/mapPage.html';
+                                                               },
+                                                               error: function(jqXHR,textStatus,errorThrown)
+                                                               {
+                                                                   console.log(textStatus,errorThrown);
+                                                                   alert("Your event could not be created at this time.");
+                                                               }
+                                                           });
+                                                   });
+      };
--- js/map.js
+++ js/map.js
@@ -1,7 +1,15 @@
+function openpad(title,timestamp,message)
+{
+    $("#MAPVIEW").hide();
+    $("#LISTVIEW").hide();
+    $("#gloopad").show();
+    $("#gloopad").text(title + timestamp + message);
+}
 window.onload = function()
 {
     $("#LISTVIEW").hide();
     $("#NEWEVENT").hide();
+    $("#gloopad").hide();
     var mapDiv = document.getElementById('map');
     navigator.geolocation.getCurrentPosition(showpos);
     function showpos(pos)
@@ -9,44 +17,25 @@ window.onload = function()
         var myLatLng = {lat: pos.coords.latitude, lng: pos.coords.longitude};
 
         var options =
-            {
-                center: myLatLng,
-                zoom: 14,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                disableDefaultUI: true,
-                navigationControl: false,
-                draggable: true,
-                scrollwheel: true
-            };
+                {
+                    center: myLatLng,
+                    zoom: 14,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    disableDefaultUI: true,
+                    navigationControl: false,
+                    draggable: false,
+                    scrollwheel: false
+                };
 
         var mapObject = new google.maps.Map(mapDiv, options);
 
-        /* write function to create marker for each event for */
-        var marker_options =
-            {
-                position: new google.maps.LatLng(myLatLng),
-                map: mapObject,
-                title: 'Your Location'
-            };
-        var marker = new google.maps.Marker(marker_options);
-
-        var infowindow_options =
-            {
-                content: "<p>title: " + title + "</p>"
-            };
-        var infowindow = new google.maps.InfoWindow(infowindow_options);
-        marker.addListener('click', function()
-                           {
-                               infowindow.open(mapObject, marker);
-                           });
 
         var event_data =
-            {
-                "lat":pos.coords.latitude,
-                "lng":pos.coords.longitude,
-                "radius":2
-            };
-
+                {
+                    "lat":pos.coords.latitude,
+                    "lng":pos.coords.longitude,
+                    "radius":2
+                };
         $.ajax(
             {
                 type: 'POST',
@@ -69,9 +58,23 @@ window.onload = function()
                                      var d = new Date();
                                      var current_time = d.getTime();
                                      var time_since = current_time - time;
-                                     var days_since = Math.round(time_since / days);
-                                     var hrs_since = Math.round(time_since / hours);
-                                     var min_since = Math.round(time_since / minutes);
+                                     var days_since = Math.floor(time_since / days);
+                                     var hrs_since = Math.floor(time_since/ hours);
+                                     var min_since = Math.floor(time_since / minutes);
+                                     var timestamp = "";
+                                     if (days_since>0)
+                                     {
+                                         timestamp = +days_since+' days, '+ (hrs_since-(days_since*24))+' hrs, '+ (min_since-(hrs_since*60)) +' minutes ago';
+                                     }
+                                     else if (hrs_since>0)
+                                     {
+                                         timestamp = +hrs_since+' hrs, '+ (min_since-(hrs_since*60)) +' minutes ago';
+                                     }
+                                     else
+                                     {
+                                         timestamp = + min_since +' minutes ago';
+                                     }
+
                                      $("#list").prepend
                                      ($(
                                          '<div class="bubbledLeft">'
@@ -79,47 +82,37 @@ window.onload = function()
                                              +'<div class="message">'
                                              +message
                                              +'<div class="timestamp">'
-                                             +'<br>'+days_since+' days, '+hrs_since+' hrs, '+ min_since +' minutes ago'
-                                         /*if (days_since>0)
-                                           {
-                                           +days_since+' days, '+hrs_since+' hrs, '+ min_since +' minutes ago'
-                                           } else if (hrs_since>0)
-                                           {
-                                           +hrs_since+' hrs, '+ min_since +' minutes ago'
-                                           } else {
-                                           + min_since +' minutes ago'
-                                           }*/
+                                             +timestamp
                                              +'</div>'
                                              +'</div>'
                                              +'</div>'
-                                     )
+                                     ).click(function(e){
+                                         openpad(title,timestamp,message);
+                                     })
                                      );
                                      var marker_options =
-                                         {
-                                             position: new google.maps.LatLng(location[1],location[0]),
-                                             map: mapObject,
-                                             title: title
-                                         };
+                                             {
+                                                 position: new google.maps.LatLng(location[1],location[0]),
+                                                 map: mapObject
+                                             };
                                      var marker = new google.maps.Marker(marker_options);
                                      var infowindow_options =
-                                         {
-                                             content: "<p>title: " + title + "</p>" +
-                                                 "<p>description: " + message + "</p>" +
-                                                 "<p>time since post: " + hrs_since + "hours " + min_since + "minutes.</p>"
-                                         };
+                                             {
+                                                 content: title + '<br>' + timestamp
+                                             };
                                      var infowindow = new google.maps.InfoWindow(infowindow_options);
-                                     marker.addListener('click', function()
-                                                        {
-                                                            infowindow.open(mapObject, marker);
-                                                        });
+                                     google.maps.event.addListener(marker, 'click', function()
+                {
+                    infowindow.open(mapObject, marker);
+                });
                                  });
                 },
                 error: function(jqXHR,textStatus,errorThrown)
                 {
                     console.log(textStatus,errorThrown);
-                    alert("Error finding events.");
+                    alert("There was an error finding events in your area.");
                 }
-            });
+      });
     };
 };
 
