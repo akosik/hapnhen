8eb95cb27e5dc125b386262cf071f9396a27e446
Alec Kosik <akosik7@gmail.com>
2016-01-13 09:24:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var router = require('./router');
 var rHs = require('./requestHandlers');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+const readline = require('readline');
+const process = require('process');
 
 //handleDict is used for routing requests to their respective handlers
 var handleDict = {};
@@ -12,11 +14,24 @@ handleDict['/createEvent'] = rHs.createEvent;
 handleDict['/editEvent'] = rHs.editEvent;
 handleDict['/deleteEvent'] = rHs.deleteEvent;
 
-//database url
-var url = 'mongodb://akosik:epsilon0@ds037415.mongolab.com:37415/events';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('Enter your MongoLab Username: ', function(answer) {
+    var user = answer;
+    console.log('Thank you for your valuable feedback:', answer);
 
-//the beginning of it all
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    server.start(router.route, handleDict, db);
+    rl.question('Password: ', function(answer) {
+        var psswd = answer;
+
+        //database url
+        var url = 'mongodb://' + user + ':' + psswd + '@ds037415.mongolab.com:37415/events';
+
+        //the beginning of it all
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            server.start(router.route, handleDict, db);
+        });
+    });
 });
